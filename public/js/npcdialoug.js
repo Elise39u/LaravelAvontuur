36885233cb208cfea1a08de238747b79fe195246
Justin van de laar <justin555@live.nl>
2017-10-21 15:29:48 +0200

    This is the correct version but sill working on a way to correct separte buttons by pressing it

--- public/js/npcdialoug.js
+++ public/js/npcdialoug.js
@@ -1,17 +1,12 @@
 $(function() {
     document.getElementById('Story').innerHTML = dialouginfo[0]['story'];
     var answerButtons = {};
-    var answerNumber = 1;
     for (var i = 1; i < 4; i++) {
         answerButtons[i] = {
             text: dialouginfo[0]['Answer_' + i],
-            click: function() {
-               alert('I`m Answer_ ' + answerNumber);
-               answerNumber++;
-            }
         };
     }
-    
+
     $("#dialog-2").dialog({
         autoOpen: false,
         width: 600,
