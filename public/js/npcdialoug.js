cfaf96fb2cf0e5bfc548639398d79ff74fbe33af
Justin van de laar <justin555@live.nl>
2017-10-21 16:28:20 +0200

    The Dialoug system works as i want it

--- public/js/npcdialoug.js
+++ public/js/npcdialoug.js
@@ -1,17 +1,31 @@
 $(function() {
     document.getElementById('Story').innerHTML = dialouginfo[0]['story'];
-    var answerButtons = {};
-    for (var i = 1; i < 4; i++) {
-        answerButtons[i] = {
-            text: dialouginfo[0]['Answer_' + i],
-            click: function () {$(this).dialog('close');}
-        };
-    }
+    var Answer1 = dialouginfo[0]['Answer_1']
+    var Answer2 = dialouginfo[0]['Answer_2']
+    var Answer3 = dialouginfo[0]['Answer_3']
 
     $("#dialog-2").dialog({
         autoOpen: false,
         width: 600,
-        buttons: answerButtons,
+        buttons: [{
+            text: Answer1,
+            "id": "Answer1",
+            click: function () {
+                $(this).dialog('close');
+            }
+        }, {
+            text: Answer2,
+            "id": "Answer2",
+            click: function () {
+                $(this).dialog('close');
+            }
+        }, {
+            text: Answer3,
+            "id": "Answer3",
+            click: function () {
+                $(this).dialog('close');
+            },
+        }],
     });
 
     $("#opener-2").click(function () {
