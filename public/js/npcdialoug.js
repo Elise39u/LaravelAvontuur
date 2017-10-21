317aa88b57b29e97395fd30756aa31cffbe3fbff
Justin van de laar <justin555@live.nl>
2017-10-21 15:24:53 +0200

    Changed the dialoug tree a bit so the button belongs with onclicks and a numeric array

--- public/js/npcdialoug.js
+++ public/js/npcdialoug.js
@@ -1,17 +1,22 @@
 $(function() {
     document.getElementById('Story').innerHTML = dialouginfo[0]['story'];
-    var checktime = function () {
-        alert('i worked');
-    }
     var answerButtons = {};
+    var answerNumber = 1;
     for (var i = 1; i < 4; i++) {
-        answerButtons[dialouginfo[0]['Answer_' + i]] = checktime;
+        answerButtons[i] = {
+            text: dialouginfo[0]['Answer_' + i],
+            click: function() {
+               alert('I`m Answer_ ' + answerNumber);
+               answerNumber++;
+            }
+        };
     }
 
+    console.log(answerButtons);
     $("#dialog-2").dialog({
         autoOpen: false,
         width: 600,
-        buttons: answerButtons
+        buttons: answerButtons,
     });
 
     $("#opener-2").click(function () {
